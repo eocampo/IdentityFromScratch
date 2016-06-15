f059382299515d62425f88696c378ae142dc99b2
José Ernesto Ocampo Cicero <ernesto@sistrategia.com>
2016-06-15 18:16:58 -0500

    Adding Empty AspNetIdentity Libraries.

---
+++ src/Libraries/Microsoft.AspNet.Identity.Core/UserManager.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation, Inc. All rights reserved.
+// Licensed under the MIT License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.AspNet.Identity.Core
+{
+    public class UserManager
+    {
+    }
+}
---
+++ src/Libraries/Microsoft.AspNet.Identity.EntityFramework/IdentityDbContext.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft Corporation, Inc. All rights reserved.
+// Licensed under the MIT License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.AspNet.Identity.EntityFramework
+{
+    public class IdentityDbContext
+    {
+    }
+}
