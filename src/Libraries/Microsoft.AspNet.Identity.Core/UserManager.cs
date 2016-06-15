f6151d1d65bbe593f2562e54dba89399af4b6b34
José Ernesto Ocampo Cicero <ernesto@sistrategia.com>
2016-06-15 18:57:34 -0500

    Basic identity classes to implement IUser with EntityFramework.

--- src/Libraries/Microsoft.AspNet.Identity.Core/UserManager.cs
+++ src/Libraries/Microsoft.AspNet.Identity.Core/UserManager.cs
@@ -9,7 +9,7 @@ using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Microsoft.AspNet.Identity.Core
+namespace Microsoft.AspNet.Identity
 {
     public class UserManager
     {
