3b3ef449587c5e74f2dc408f3955ff5be5e68d62
José Ernesto Ocampo Cicero <ernesto@sistrategia.com>
2016-06-15 20:06:29 -0500

    Adding UserStore (Data Access Layer complete).

--- src/Application/IdentityFromScratchWebApp/App_Start/Startup.Auth.cs
+++ src/Application/IdentityFromScratchWebApp/App_Start/Startup.Auth.cs
@@ -6,6 +6,7 @@ namespace IdentityFromScratchWebApp
     public partial class Startup
     {
         public void ConfigureAuth(IAppBuilder app) {
+            // app.CreatePerOwinContext(ApplicationDbContext.Create);
         }
     }
 }
---
+++ src/Application/IdentityFromScratchWebApp/Controllers/DevController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IdentityFromScratchWebApp.Models;
+using System.Data.Entity;
+
+namespace IdentityFromScratchWebApp.Controllers
+{
+    public class DevController : Controller
+    {
+        // GET: Dev
+        public ActionResult Index() {
+            return View();
+        }
+
+        public ActionResult CreateDatabase() {
+            ApplicationDbContext dbContext = ApplicationDbContext.Create();
+            dbContext.Roles.Add(new Microsoft.AspNet.Identity.EntityFramework.IdentityRole("Developers"));
+            return Redirect("~/Dev");
+        }
+
+        public ActionResult CreateSchema() {
+            //var configuration = new Migrations.Configuration();
+            //var migrator = new System.Data.Entity.Migrations.DbMigrator(configuration);
+            //migrator.Update();
+            return Redirect("~/Dev");
+        }
+
+        public ActionResult DropSchema() {
+            //var configuration = new Migrations.Configuration();
+            //var migrator = new System.Data.Entity.Migrations.DbMigrator(configuration);
+            //migrator.Update("0");
+            return Redirect("~/Dev");
+        }
+    }
+}
