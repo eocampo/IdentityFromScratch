c80e42c2f7aa93416625d4f6949b5f689fa61813
José Ernesto Ocampo Cicero <ernesto@sistrategia.com>
2016-06-15 19:30:41 -0500

    Adding RoleStore.

---
+++ src/Libraries/Microsoft.AspNet.Identity.EntityFramework/GlobalSuppressions.cs
@@ -0,0 +1,70 @@
+﻿// Copyright (c) Microsoft Corporation, Inc. All rights reserved.
+// Licensed under the MIT License, Version 2.0. See License.txt in the project root for license information.
+
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+//
+// To add a suppression to this file, right-click the message in the 
+// Code Analysis results, point to "Suppress Message", and click 
+// "In Suppression File".
+// You do not need to add suppressions to this file manually.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly:
+    SuppressMessage("Microsoft.Design", "CA1005:AvoidExcessiveParametersOnGenericTypes", Scope = "type",
+        Target = "Microsoft.AspNet.Identity.EntityFramework.UserStore`6")]
+[assembly:
+    SuppressMessage("Microsoft.Naming", "CA1726:UsePreferredTerms", MessageId = "Login", Scope = "type",
+        Target = "Microsoft.AspNet.Identity.EntityFramework.UserStore`6")]
+[assembly:
+    SuppressMessage("Microsoft.Design", "CA1005:AvoidExcessiveParametersOnGenericTypes", Scope = "type",
+        Target = "Microsoft.AspNet.Identity.EntityFramework.RoleStore`3")]
+[assembly:
+    SuppressMessage("Microsoft.Design", "CA1063:ImplementIDisposableCorrectly", Scope = "type",
+        Target = "Microsoft.AspNet.Identity.EntityFramework.RoleStore`1")]
+[assembly:
+    SuppressMessage("Microsoft.Naming", "CA1726:UsePreferredTerms", MessageId = "Login", Scope = "member",
+        Target = "Microsoft.AspNet.Identity.EntityFramework.IdentityUserLogin`1.#LoginProvider")]
+[assembly:
+    SuppressMessage("Microsoft.Naming", "CA1726:UsePreferredTerms", MessageId = "Login", Scope = "type",
+        Target = "Microsoft.AspNet.Identity.EntityFramework.IdentityUserLogin`1")]
+[assembly:
+    SuppressMessage("Microsoft.Naming", "CA1726:UsePreferredTerms", MessageId = "Login", Scope = "type",
+        Target = "Microsoft.AspNet.Identity.EntityFramework.IdentityUserLogin")]
+[assembly:
+    SuppressMessage("Microsoft.Design", "CA1005:AvoidExcessiveParametersOnGenericTypes", Scope = "type",
+        Target = "Microsoft.AspNet.Identity.EntityFramework.IdentityUser`4")]
+[assembly:
+    SuppressMessage("Microsoft.Naming", "CA1726:UsePreferredTerms", MessageId = "Login", Scope = "type",
+        Target = "Microsoft.AspNet.Identity.EntityFramework.IdentityUser`4")]
+[assembly:
+    SuppressMessage("Microsoft.Design", "CA1005:AvoidExcessiveParametersOnGenericTypes", Scope = "type",
+        Target = "Microsoft.AspNet.Identity.EntityFramework.IdentityDbContext`6")]
+[assembly:
+    SuppressMessage("Microsoft.Naming", "CA1726:UsePreferredTerms", MessageId = "Login", Scope = "type",
+        Target = "Microsoft.AspNet.Identity.EntityFramework.IdentityDbContext`6")]
+[assembly:
+    SuppressMessage("Microsoft.Design", "CA1063:ImplementIDisposableCorrectly", Scope = "type",
+        Target = "Microsoft.AspNet.Identity.EntityFramework.UserStore`1")]
+[assembly:
+    SuppressMessage("Microsoft.Naming", "CA1726:UsePreferredTerms", MessageId = "Login", Scope = "type",
+        Target = "Microsoft.AspNet.Identity.EntityFramework.IdentityDbContext`7")]
+[assembly:
+    SuppressMessage("Microsoft.Naming", "CA1726:UsePreferredTerms", MessageId = "Login", Scope = "type",
+        Target = "Microsoft.AspNet.Identity.EntityFramework.IdentityUser`5")]
+[assembly:
+    SuppressMessage("Microsoft.Design", "CA1005:AvoidExcessiveParametersOnGenericTypes", Scope = "type",
+        Target = "Microsoft.AspNet.Identity.EntityFramework.IdentityUser`5")]
+[assembly:
+    SuppressMessage("Microsoft.Naming", "CA1726:UsePreferredTerms", MessageId = "Login", Scope = "type",
+        Target = "Microsoft.AspNet.Identity.EntityFramework.UserStore`7")]
+[assembly:
+    SuppressMessage("Microsoft.Design", "CA1005:AvoidExcessiveParametersOnGenericTypes", Scope = "type",
+        Target = "Microsoft.AspNet.Identity.EntityFramework.UserStore`7")]
+[assembly:
+    SuppressMessage("Microsoft.Design", "CA1005:AvoidExcessiveParametersOnGenericTypes", Scope = "type",
+        Target = "Microsoft.AspNet.Identity.EntityFramework.IdentityDbContext`7")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures", Scope = "member", Target = "Microsoft.AspNet.Identity.EntityFramework.UserStore`6.#GetUserAggregateAsync(System.Linq.Expressions.Expression`1<System.Func`2<!0,System.Boolean>>)")]
